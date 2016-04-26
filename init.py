27e329754d38cbe21c6230bf540785fd00ae28c9
Preston Cammarata <prestotron7@gmail.com>
2016-04-26 11:50:29 -0400

    update the json api lol

--- init.py
+++ init.py
@@ -3,22 +3,22 @@
 
 # weatherlogr by Preston Cammarata (c) 2016
 
+# sudo apt-get install python-pip
+# pip install tweepy
+
+
 import tweepy, time, sys, urllib2, json;
 
 # Variables
 TOWN       = "North_Kingstown";  # Your town, with '_' replacing spaces
 STATE      = "RI";               # Your state (ex: CA, RI, MA, etc.)
-API_KEY    = "YourKeyHere";      # Your Wunderground API key
-
-# Fetch the API
-_API       = urllib2.urlopen( 'http://api.wunderground.com/api/%s/geolookup/conditions/q/%s/%s.json' % ( API_KEY, STATE, TOWN ) );
-API        = json.loads ( _API.read ( ) );
+API_KEY    = "yourKeyHere"; # Your Wunderground API key
 
 # Twitter API
-CONSUMER_KEY    = 'YourKeyHere';
-CONSUMER_SECRET = 'YourKeyHere';
-ACCESS_KEY      = 'YourKeyHere';
-ACCESS_SECRET   = 'YourKeyHere';
+CONSUMER_KEY    = 'yourKeyHere';
+CONSUMER_SECRET = 'yourKeyHere';
+ACCESS_KEY      = 'yourKeyHere';
+ACCESS_SECRET   = 'yourKeyHere';
 
 auth = tweepy.OAuthHandler ( CONSUMER_KEY, CONSUMER_SECRET );
 auth.set_access_token ( ACCESS_KEY, ACCESS_SECRET );
@@ -26,12 +26,18 @@ api  = tweepy.API ( auth );
 
 
 while ( 1 ):
+	# Fetch the API
+	_API       = urllib2.urlopen( 'http://api.wunderground.com/api/%s/geolookup/conditions/q/%s/%s.json' % ( API_KEY, STATE, TOWN ) );
+	API        = json.loads ( _API.read ( ) );
+	
+	# Post to Twitter
 	MESSAGE = "weather: %s temp: %s humidity: %s wind speed: %s" % ( API['current_observation']['weather'], API['current_observation']['temp_f'], API['current_observation']['relative_humidity'], API['current_observation']['wind_mph'] );
 	try:
 		api.update_status ( MESSAGE );
 	except:
 		pass
 	time.sleep ( 3600 );
+
 
 # Close the API connection
 _API.close( );
